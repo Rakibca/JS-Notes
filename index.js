26633a77cf33451cca86dd6436d58e2ddf12ad9a
Rakibul Islam <rakib.ca@gmail.com>
2026-02-26 23:21:55 -0700

    code updates

--- index.js
+++ index.js
@@ -113,3 +113,28 @@ let html = `
 `;
 console.log(html);
 // ======================================================
+// 13 - arrays
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+console.log(ninjas);
+console.log(ninjas[1]); // ryu
+ninjas[1] = 'ken';
+console.log(ninjas[1]); // ken
+
+let ages = [20, 25, 30, 35];
+console.log(ages[2]);
+
+let random = ['shaun', 'crystal', 30, 20];
+console.log(random);
+
+// array length
+console.log(ninjas.length); // 3
+
+// array methods
+let result = ninjas.join(',');
+let result = ninjas.indexOf('chun-li');
+let result = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+let result = ninjas.pop();
+console.log(result);
+console.log(ninjas);
+// ======================================================
