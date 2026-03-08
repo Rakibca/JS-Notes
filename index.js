5cfb223fa2743cd98ad94108abb8d955f0e3336f
Rakibul Islam <rakib.ca@gmail.com>
2026-03-07 23:41:53 -0700

    code updates

--- index.js
+++ index.js
@@ -177,9 +177,9 @@ age = 25;
 
 // loose comparison (different types can still be equal)
 console.log(age == 25); // true
-console.log(age == '25');
-console.log(age != 25);
-console.log(age != '25');
+console.log(age == '25'); // true
+console.log(age != 25); // false
+console.log(age != '25'); // false
 
 // strict comparison (different types cannot be equal)
 console.log(age === 25);
