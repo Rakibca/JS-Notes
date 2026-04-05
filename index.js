737afaf0362a2f1041f13e8e522412003887f2ab
Rakibul Islam <rakib.ca@gmail.com>
2026-04-04 22:40:33 -0600

    code updates

--- index.js
+++ index.js
@@ -262,3 +262,14 @@ if (password.length >= 8) {
   console.log('that password is long enough');
 }
 // ======================================================
+// 23 - else if statements
+const password2 = 'p@ssword123456';
+
+if (password2.length >= 12) {
+  console.log('that password is mighty strong');
+} else if (password2.length >= 8) {
+  console.log('that password is long enough');
+} else {
+  console.log('password should be at least 8 characters long');
+}
+// ======================================================
