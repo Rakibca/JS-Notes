606fc2cbe9eb7285216bcd9359967a114cfd0a87
Rakibul Islam <rakib.ca@gmail.com>
2026-06-10 11:22:49 -0600

    code updates

--- index.js
+++ index.js
@@ -340,18 +340,14 @@ if (grade === 'A') {
 // ======================================================
 // 28 - variables and block scope
 let age = 30;
-
 if (true) {
-  // age = 40;
   let age = 40;
   let name = 'shaun';
   console.log('inside 1st code block:', age, name);
-
   if (true) {
     let age = 50;
     console.log('inside 2nd code block:', age, name);
   }
 }
-
 console.log('outside code block:', age, name);
 // ======================================================
