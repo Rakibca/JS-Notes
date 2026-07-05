ab08151e6df10c0f37335a2f0467fb8e4706cd2b
Rakibul Islam <rakib.ca@gmail.com>
2026-07-05 15:09:24 -0600

    code updates

--- index.js
+++ index.js
@@ -388,14 +388,13 @@ console.log('area is:', area);
 // ======================================================
 // 33 - arrow functions
 
-// regular function
-// const calcArea = function(radius){
-//   return 3.14 * radius**2;
-// }
+// This is a regular function
+/*const calcArea = function (radius) {
+  return 3.14 * radius ** 2;
+};*/
 
-// arrow function
+// This is an arrow function
 const calcArea = (radius) => 3.14 * radius ** 2;
-
 const area = calcArea(5);
 console.log('area is:', area);
 
