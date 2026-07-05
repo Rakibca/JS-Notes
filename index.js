a0fd7aed451bf007d2b13f081a075f61651f5637
Rakibul Islam <rakib.ca@gmail.com>
2026-07-04 19:38:04 -0600

    code updates

--- index.js
+++ index.js
@@ -384,9 +384,8 @@ speak('shaun');
 //   console.log(`good ${time}, ${name}!`);
 // };
 const calcArea = function (radius) {
-  let area = 3.14 * radius ** 2;
-  return area;
+  return 3.14 * radius ** 2;
 };
-const area = calcArea(5);
+const area = calcArea(7);
 console.log('area is:', area);
 // ======================================================
