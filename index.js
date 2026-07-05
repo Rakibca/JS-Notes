926587fb6274c38a7f718952504907b5d89842f1
Rakibul Islam <rakib.ca@gmail.com>
2026-07-05 15:15:50 -0600

    code updates

--- index.js
+++ index.js
@@ -394,25 +394,24 @@ console.log('area is:', area);
 };*/
 
 // This is an arrow function
-const calcArea = radius => 3.14 * radius ** 2;
+const calcArea = (radius) => 3.14 * radius ** 2;
 const area = calcArea(5);
 console.log('area is:', area);
 
-// practise arrow functions
-// const greet = function(){
-//   return 'hello, world';
-// }
-
+// Another regular function
+/*const greet = function () {
+  return 'hello, world';
+};*/
 const greet = () => 'hello, world';
 
-// const bill = function(products, tax){
-//   let total = 0;
-//   for(let i = 0; i < products.length; i++){
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// }
-
+// Another regular function
+/*const bill = function (products, tax) {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+};*/
 const bill = (products, tax) => {
   let total = 0;
   for (let i = 0; i < products.length; i++) {
